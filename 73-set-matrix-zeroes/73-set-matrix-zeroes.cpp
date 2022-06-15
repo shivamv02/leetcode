01d515a960213a1ac74faf9d0058a15f754e4f6b
shivamv02 <91478433+shivamv02@users.noreply.github.com>
2022-06-15 22:46:29 +0530

    Time: 16 ms (78.82%), Space: 13.1 MB (95.78%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -9,16 +9,12 @@ public:
                     v.push_back({i,j});
             }
         for(auto x:v)
-        for(int i=0;i<m.size();i++)
-            for(int j=0;j<m[0].size();j++)
-            {
-                if(x.first==i and x.second ==j)
-                {
-                    for(int k=0;k<m[0].size();k++)
+        { int i=x.first,j=x.second;
+                 for(int k=0;k<m[0].size();k++)
                         m[i][k]=0;
                 for(int l=0;l<m.size();l++)
                     m[l][j]=0;
-                }
-            }
+                
+        }
     }
 };
