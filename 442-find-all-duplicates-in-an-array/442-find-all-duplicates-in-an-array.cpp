e108261d13f38d5e0cb0900146632b47b7bcc14b
shivamv02 <91478433+shivamv02@users.noreply.github.com>
2022-06-17 00:58:58 +0530

    Time: 67 ms (79.99%), Space: 33.6 MB (79.61%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             if(nums[abs(nums[i])-1]>0)
-                nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
+                nums[abs(nums[i])-1]*=-1;
             else
                 v.push_back(abs(nums[i]));
         }
