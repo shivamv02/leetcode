d4bf69663f897536385f75cfcb80bd412b2217e6
shivamv02 <91478433+shivamv02@users.noreply.github.com>
2022-06-19 22:09:51 +0530

    Time: 21 ms (82.42%), Space: 19.6 MB (58.84%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        
+        // Moore's Voting Algorithm
+        
         int el=nums[0],cnt=1;
         for(int i=1;i<nums.size();i++)
         {
