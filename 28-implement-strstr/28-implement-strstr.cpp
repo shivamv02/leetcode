2cc12832efa5e02cd7be5cafd63c1e338c36dd6b
shivamv02 <91478433+shivamv02@users.noreply.github.com>
2022-06-20 21:26:15 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (90.21%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        size_t found=haystack.find(needle);
-        if(found!=string::npos)
-            return found;
+        if(haystack.find(needle)!=string::npos)
+            return haystack.find(needle);
         else return -1;
     }
 };
