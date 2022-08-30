9f40353400e89fe4c08ebf4fd11519e413e4e1f7
shivamv02 <91478433+shivamv02@users.noreply.github.com>
2022-08-30 21:26:13 +0530

    Time: 17 ms (79.15%), Space: 13.4 MB (18.98%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& m) {
-       
-        int col=1;
-    
-        for(int i=0;i<m.size();i++)
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<matrix.size();i++)
+            for(int j=0;j<matrix[0].size();j++)
+                if(matrix[i][j]==0)
+                v.push_back({i,j});
+        for(auto x:v)
         {
-            if(m[i][0]==0) col=0;
-            for(int j=1;j<m[0].size();j++)
-            if(m[i][j]==0)
-                m[0][j]=m[i][0]=0; 
+            for(int i=0;i<matrix[0].size();i++)
+                matrix[x.first][i]=0;
+             for(int i=0;i<matrix.size();i++)
+                matrix[i][x.second]=0;
+        
         }
-        for(int i=m.size()-1;i>=0;i--)
-        { 
-            for(int j=m[0].size()-1;j>=1;j--)
-            {
-                if(m[i][j]!=0 and (m[0][j]==0 or m[i][0]==0))
-                    m[i][j]=0;
-              
-            }
-            if(col==0)
-            m[i][0]=0;
-        }
-     
-          
         
     }
 };
